cea771c061335bbf258ad83743af61eaea7f50ef
waahah <90046731+waahah@users.noreply.github.com>
2023-04-27 00:28:09 +0800

    update line 6

--- src/components/mm-header/mm-header.vue
+++ src/components/mm-header/mm-header.vue
@@ -3,7 +3,7 @@
   <header class="mm-header">
     <h1 class="header">
       <a href="https://github.com/maomao1996/Vue-mmPlayer" target="_blank">
-        mmPlayer 在线音乐播放器
+        mmPlayer 云音乐在线播放器
       </a>
       <img
         v-if="visitorBadge"
