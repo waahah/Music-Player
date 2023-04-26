8a9bef941019608f3e376a689589f6940992388d
waahah <90046731+waahah@users.noreply.github.com>
2023-04-26 23:37:18 +0800

    Update music.vue

--- src/pages/music.vue
+++ src/pages/music.vue
@@ -29,7 +29,7 @@
             {{ currentMusic.name }}
             <span>- {{ currentMusic.singer }}</span>
           </template>
-          <template v-else>欢迎使用mmPlayer在线音乐播放器</template>
+          <template v-else>欢迎使用waahah云音乐在线播放器</template>
         </div>
         <div v-if="currentMusic.id" class="music-bar-time">
           {{ currentTime | format }}/{{ currentMusic.duration % 3600 | format }}
